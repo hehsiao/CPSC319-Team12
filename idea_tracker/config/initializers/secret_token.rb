9e071ea362ac26b3a3240bda7646624b68b6eeb3
Ilja Bryliov <bryliov@gmail.com>
2014-02-17 20:51:57 -0800

    New Project - no modifications yet

---
+++ idea_tracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdeaTracker::Application.config.secret_key_base = '4d28a9a9eb8db5e6fd07550827d7ce744f8d0fd8607a52dac044a9125eb21ae979f5d184b4836b6603268f31974de37788940a42167a0a4894aa0ea59c702af1'
