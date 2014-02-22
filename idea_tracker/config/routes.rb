4367763ede2fe69cbe523540e63933538bb04bcb
Ilja Bryliov <bryliov@gmail.com>
2014-02-21 23:12:15 -0800

    implemented user authentication and admin user. Separate routing to user's and admin's dashboards

--- idea_tracker/config/routes.rb
+++ idea_tracker/config/routes.rb
@@ -1,4 +1,9 @@
 IdeaTracker::Application.routes.draw do
+  devise_for :users
+  resources :dashboard
+  resources :admin
+
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
