7f54629b078142d26bde1f7791c75716dcf2751a
Tanat Ratanakosum <tanat.ratana@gmail.com>
2014-03-18 15:08:18 -0700

    notification for create idea and email for registering

--- idea_tracker/app/controllers/dashboard_controller.rb
+++ idea_tracker/app/controllers/dashboard_controller.rb
@@ -3,9 +3,11 @@ class DashboardController < ApplicationController
 	before_filter :authenticate_user!
  
 	def index  
-		@ideas = Idea.all  
-		@recent_ideas = Idea.recent
+		@ideas = Idea.all
+		@sub_ideas = Subscription.where(user_id: current_user.id)    
 		#@notifications = Notification.all
+		@activities = PublicActivity::Activity.order("created_at desc")
+		@recent_ideas = Idea.recent
 	end
 
 end
