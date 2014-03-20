eb7f50e1f8a0bc634e30ffc5b694b7361f0c7591
Tanat Ratanakosum <tanat.ratana@gmail.com>
2014-03-20 01:47:07 -0700

    merging

--- idea_tracker/db/schema.rb
+++ idea_tracker/db/schema.rb
@@ -30,23 +30,6 @@ ActiveRecord::Schema.define(version: 20140320074458) do
   add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
   add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
-  create_table "activities", force: true do |t|
-    t.integer  "trackable_id"
-    t.string   "trackable_type"
-    t.integer  "owner_id"
-    t.string   "owner_type"
-    t.string   "key"
-    t.text     "parameters"
-    t.integer  "recipient_id"
-    t.string   "recipient_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
-
   create_table "associations", id: false, force: true do |t|
     t.integer  "parent_idea_id"
     t.integer  "tagged_idea_id"
@@ -78,19 +61,18 @@ ActiveRecord::Schema.define(version: 20140320074458) do
     t.datetime "updated_at"
   end
 
-<<<<<<< HEAD
   create_table "conversations", force: true do |t|
     t.string   "subject",    default: ""
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-=======
+  end
+
   create_table "idea_tags", id: false, force: true do |t|
     t.integer  "category_id"
     t.integer  "idea_id"
     t.string   "additional_text"
     t.datetime "created_at"
     t.datetime "updated_at"
->>>>>>> 5ced37aab105f2a896513074056437e3eff1e094
   end
 
   create_table "ideas", force: true do |t|
