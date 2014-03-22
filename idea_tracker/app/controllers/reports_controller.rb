db4401cd2a663e6e8a029ad5b57bed1463a55ef4
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2014-03-22 10:44:18 -0700

    improved success rate report
    the displayed success rate is no longer hardcoded
    can view the ideas owned by each user

--- idea_tracker/app/controllers/reports_controller.rb
+++ idea_tracker/app/controllers/reports_controller.rb
@@ -4,6 +4,8 @@ class ReportsController < ApplicationController
   
   def success_rate
    @users = User.all
+   @ideas = Idea.all
+   @status_size = Status.all.size
  end
  
  def status
@@ -18,10 +20,17 @@ class ReportsController < ApplicationController
  def status_show
    @ideas = Idea.where(status: params[:id])
    @status = Status.find(params[:id])
+   @all_status = Status.all
  end
  
  def status_pie
    @status = Status.all
    @ideas = Idea.all
+ end
+ 
+ def success_rate_show
+	@ideas = Idea.where(user_id: params[:id])
+	@user = User.find(params[:id])
+	@all_status = Status.all
  end
 end
