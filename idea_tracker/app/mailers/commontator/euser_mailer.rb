a30033366a81a4b8e3d9ea01681108e7b35d8d04
Tanat Ratanakosum <tanat.ratana@gmail.com>
2014-03-29 22:27:53 -0700

    comment email and fix nill on to_csv_all

--- idea_tracker/app/mailers/commontator/euser_mailer.rb
+++ idea_tracker/app/mailers/commontator/euser_mailer.rb
@@ -1,23 +1,24 @@
-class EuserMailer < ActionMailer::Base
-  default from: "cpsc3192014@gmail.com"
+module Commontator
+    class EuserMailer < ActionMailer::Base
+      default from: "cpsc3192014@gmail.com"
 
-    def comment_email(thread, user, comment_context)
-    @user = user
-    @thread = thread
-    @idea = Idea.where(idea_id: thread.commontable_id)
-    @comment = comment_context
-    @related_users = Subscription.where(idea_id: thread.commontable_id)
-    @all_email = []
+     def comment_email(thread, user, comment_context)
+        @user = user
+        @thread = thread
+        @idea = ::Idea.where(idea_id: thread.commontable_id)
+        @comment = comment_context
+        @related_users = ::Subscription.where(idea_id: @thread.commontable_id)
+        @all_email = []
 
-    @related_users.each do |related_user|
-    if User.find_by_id(related_user.user_id)      
-    @all_email << User.find_by_id(related_user.user_id).email  
-    end 
+        @related_users.each do |related_user|
+        if ::User.find_by_id(related_user.user_id)      
+        @all_email << ::User.find_by_id(related_user.user_id).email  
+        end 
+        end
+
+        if @all_email.present?
+        mail(to: @all_email, subject: 'Someone post a comment on Ideal Tracker')
+        end
+      end
     end
-
-    if @all_email.present?
-    mail(to: @all_email, subject: 'Someone post a comment on Ideal Tracker')
-    end
-  end
-
 end
--- idea_tracker/app/mailers/user_mailer.rb
+++ idea_tracker/app/mailers/user_mailer.rb
@@ -39,4 +39,22 @@ class UserMailer < ActionMailer::Base
     #flash[:notice] = "sended successfully"
   end
 
+  def comment_email(thread, user, comment_context)
+    @user = user
+    @thread = thread
+    @idea = Idea.find_by_id(@thread.commontable_id)
+    @comment = comment_context
+    @related_users = Subscription.where(idea_id: @thread.commontable_id)
+    @all_email = []
+
+    @related_users.each do |related_user|
+      if User.find_by_id(related_user.user_id)      
+        @all_email << User.find_by_id(related_user.user_id).email  
+      end 
+    end
+
+    if @all_email.present?
+      mail(to: @all_email, subject: 'Someone post a comment on Ideal Tracker')
+    end
+  end
 end
