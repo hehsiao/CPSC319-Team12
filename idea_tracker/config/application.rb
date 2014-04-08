ce71c2a34828b93bbbbbb272b6cd5c4fbf43b1eb
Henry Hsiao <me@henrychsiao.com>
2014-04-08 16:13:59 -0700

    update

--- idea_tracker/config/application.rb
+++ idea_tracker/config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'csv'
 require 'rails/all'
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -21,5 +20,7 @@ module IdeaTracker
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.enforce_available_locales = false
+
   end
 end
